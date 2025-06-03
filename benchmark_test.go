6066606b782de538022e05d1d56a9a7471dc5761
Paul Hewlett <1104895+eccles@users.noreply.github.com>
2025-06-03 10:39:08 +0100

    Generics (#4)
    Use generics to conflate string AND int lists.
    
    Signed-off-by: Paul Hewlett <phewlett76@gmail.com>

--- benchmark_test.go
+++ benchmark_test.go
@@ -4,14 +4,26 @@ import (
 	"testing"
 )
 
-func BenchmarkHead(b *testing.B) {
+func BenchmarkHead_int(b *testing.B) {
 	for b.Loop() {
 		Head([]int{10, 20, 20, 20, 30})
 	}
 }
 
-func BenchmarkConflate(b *testing.B) {
+func BenchmarkHead_string(b *testing.B) {
+	for b.Loop() {
+		Head([]string{"10", "20", "20", "20", "30"})
+	}
+}
+
+func BenchmarkConflate_int(b *testing.B) {
 	for b.Loop() {
 		Conflate([]int{10, 20, 20, 20, 30}, 2)
+	}
+}
+
+func BenchmarkConflate_string(b *testing.B) {
+	for b.Loop() {
+		Conflate([]string{"10", "20", "20", "20", "30"}, 2)
 	}
 }
