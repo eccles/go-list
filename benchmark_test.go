bb5dbce535ba24afc3720f12c6d00e38b7ff856b
Paul Hewlett <phewlett76@gmail.com>
2025-06-29 12:21:12 +0100

    Remove Head method
    Head is too small to be in separate package.
    Inline head into Conflate.
    Revisit benchmarking.
    
    Signed-off-by: Paul Hewlett <phewlett76@gmail.com>

--- benchmark_test.go
+++ benchmark_test.go
@@ -4,26 +4,42 @@ import (
 	"testing"
 )
 
-func BenchmarkHead_int(b *testing.B) {
+// b.Loop is used but there may be some drawbacks:
+//
+// https://github.com/golang/go/issues/73137
+
+func BenchmarkConflate_int_leadingduplicates(b *testing.B) {
 	for b.Loop() {
-		Head([]int{10, 20, 20, 20, 30})
+		Conflate([]int{20, 20, 20, 30, 40, 50, 60}, 2)
 	}
 }
 
-func BenchmarkHead_string(b *testing.B) {
+func BenchmarkConflate_int_midduplicates(b *testing.B) {
 	for b.Loop() {
-		Head([]string{"10", "20", "20", "20", "30"})
+		Conflate([]int{10, 20, 20, 20, 30, 40, 50, 60}, 2)
 	}
 }
 
-func BenchmarkConflate_int(b *testing.B) {
+func BenchmarkConflate_int_trailingduplicates(b *testing.B) {
 	for b.Loop() {
-		Conflate([]int{10, 20, 20, 20, 30}, 2)
+		Conflate([]int{10, 40, 50, 60, 20, 20, 20}, 2)
 	}
 }
 
-func BenchmarkConflate_string(b *testing.B) {
+func BenchmarkConflate_string_leadingduplicates(b *testing.B) {
 	for b.Loop() {
-		Conflate([]string{"10", "20", "20", "20", "30"}, 2)
+		Conflate([]string{"20", "20", "20", "30", "40", "50", "60"}, 2)
+	}
+}
+
+func BenchmarkConflate_string_midduplicates(b *testing.B) {
+	for b.Loop() {
+		Conflate([]string{"10", "20", "20", "20", "30", "40", "50", "60"}, 2)
+	}
+}
+
+func BenchmarkConflate_string_trailingduplicates(b *testing.B) {
+	for b.Loop() {
+		Conflate([]string{"10", "40", "50", "60", "20", "20", "20"}, 2)
 	}
 }
